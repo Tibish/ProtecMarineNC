1a13eb11b856ee079edc7fbd57cb67e34dfd6ab6
Tibish <138960077+Tibish@users.noreply.github.com>
2024-11-19 14:18:02 +1100

    Update ProtecMarine_Wifi.cpp

--- ProtecMarine_Wifi/ProtecMarine_Wifi.cpp
+++ ProtecMarine_Wifi/ProtecMarine_Wifi.cpp
@@ -23,24 +23,84 @@ void ProtecMarine_Wifi::setupWifi() {
     Serial.println("Serveur web démarré");
 }
 
+void ProtecMarine_Wifi::disconnectWifi() {
+    WiFi.softAPdisconnect(true);
+}
+
 void ProtecMarine_Wifi::handleRequests() {
     _server.handleClient();
 }
 
 void ProtecMarine_Wifi::handleRoot() {
-    String htmlPage = "<!DOCTYPE html><html lang=\"fr\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>ProtecMarineNC</title><style>";
-    htmlPage += "h1{text-align: center;font-family: Arial, Helvetica, sans-serif;}p{text-align: center;font-size: x-large;font-family: Arial, Helvetica, sans-serif;}button{font-size: 18px;padding: 10px 20px;display: block;margin: 10px auto;}#etat{text-align: center;background-color: #007bff;color: white;padding: 10px 20px;margin : 10px auto;}#arretButton{background-color: red;color: white;}#tension, #pression{background-color: #f0f0f0;padding: 10px;margin-top: 20px;text-align: center;border-radius: 5px;}</style></head><body>";
-    htmlPage += "<img src=\"logo-ProtecMarineNC3.png\" alt=\"Image Description\" style=\"display: block; margin: 0 auto;\">";
-    htmlPage += "<h1>Protection de coque</h1><div id=\"dataDisplay\"><p>Tension : </p><p><span id=\"tension\"></span> V</p><p>Pression :</p><p> <span id=\"pression\"></span> bars</p><p>ID : <span id=\"id\"></span></p><p><span id=\"etat\"></span></p><p>Alerte : <span id=\"alerte\"></span></p></div>";
-    htmlPage += "<button onclick=\"sendAction('/gonflage')\">Gonflage</button><button onclick=\"sendAction('/degonflage')\">Dégonflage</button><button id=\"arretButton\" onclick=\"sendAction('/arret')\">Arrêt</button>";
-    htmlPage += "<script>function getData() {var xhttp = new XMLHttpRequest();xhttp.onreadystatechange = function() {if (this.readyState == 4 && this.status == 200) {";
-    htmlPage += "var data = JSON.parse(this.responseText);document.getElementById('tension').innerHTML = data.tension;document.getElementById('alerte').innerHTML = data.alerte;";
-    htmlPage += "document.getElementById('pression').innerHTML = data.pression;document.getElementById('id').innerHTML = data.id;document.getElementById('etat').innerHTML = data.etat;}};";
-    htmlPage += "xhttp.open('GET', '/getdata', true);xhttp.send();}function sendAction(actionUrl) {var xhttp = new XMLHttpRequest();xhttp.open('GET', actionUrl, true);xhttp.send();}";
-    htmlPage += "setInterval(getData, 1000);</script></body></html>";
+  String htmlPage = R"rawliteral(
+    <!DOCTYPE html>
+    <html lang="fr">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>ProtecMarineNC</title>
+      <style>
+        h1 { text-align: center; font-family: Arial, Helvetica, sans-serif; }
+        p { text-align: center; font-size: x-large; font-family: Arial, Helvetica, sans-serif; }
+        button { font-size: 18px; padding: 10px 20px; display: block; margin: 10px auto; }
+        #etat { text-align: center; background-color: #007bff; color: white; padding: 10px 20px; margin: 10px auto; }
+        #arretButton { background-color: red; color: white; }
+        #tension, #pression { background-color: #f0f0f0; padding: 10px; margin-top: 20px; text-align: center; border-radius: 5px; }
+        svg { display: block; margin: 0 auto; max-width: 100%; height: auto; }
+      </style>
+    </head>
+    <body>
+      <svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="260.000000pt" height="90.000000pt" viewBox="0 0 260.000000 90.000000" preserveAspectRatio="xMidYMid meet">
+        <g transform="translate(0.000000,90.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none">
+          <!-- SVG paths ici -->
+        </g>
+      </svg>
+      <h1>Protection de coque</h1>
+      <div id="dataDisplay">
+        <p>Tension : </p>
+        <p><span id="tension"></span> V</p>
+        <p>Pression :</p>
+        <p><span id="pression"></span> bars</p>
+        <p>ID : <span id="id"></span></p>
+        <p><span id="etat"></span></p>
+        <p>Alerte : <span id="alerte"></span></p>
+      </div>
 
-    _server.send(200, "text/html", htmlPage);
-}
+      <button onclick="sendAction('/gonflage')">Gonflage</button>
+      <button onclick="sendAction('/degonflage')">Dégonflage</button>
+      <button id="arretButton" onclick="sendAction('/arret')">Arrêt</button>
+
+      <script>
+        function getData() {
+          var xhttp = new XMLHttpRequest();
+          xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+              var data = JSON.parse(this.responseText);
+              document.getElementById('tension').innerHTML = data.tension;
+              document.getElementById('alerte').innerHTML = data.alerte;
+              document.getElementById('pression').innerHTML = data.pression;
+              document.getElementById('id').innerHTML = data.id;
+              document.getElementById('etat').innerHTML = data.etat;
+            }
+          };
+          xhttp.open('GET', '/getdata', true);
+          xhttp.send();
+        }
+
+        function sendAction(actionUrl) {
+          var xhttp = new XMLHttpRequest();
+          xhttp.open('GET', actionUrl, true);
+          xhttp.send();
+        }
+
+        setInterval(getData, 1000);  // Mise à jour des données toutes les secondes
+      </script>
+    </body>
+    </html>
+  )rawliteral";
+
+  _server.send(200, "text/html", htmlPage);
+} 
 
 void ProtecMarine_Wifi::handleGetData() {
     String data = GetData();
